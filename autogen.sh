a70eb477732d2e8584af59389b4909541cf5bc98
Simon Feltman <sfeltman@src.gnome.org>
2014-05-20 00:33:01 -0700

    Make gtk-doc truly optional (and off by default)

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,31 @@
-#!/bin/sh -e
-gtkdocize || exit 1
-${AUTORECONF:-autoreconf} -fiv
+#!/bin/sh
+
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
+olddir=`pwd`
+cd "$srcdir"
+
+ACLOCAL_FLAGS="-I m4 ${ACLOCAL_FLAGS}"
+
+GTKDOCIZE=`which gtkdocize`
+if test -z $GTKDOCIZE; then
+    echo "*** No GTK-Doc found, documentation won't be generated ***"
+    echo 'EXTRA_DIST =' > gtk-doc.make
+else
+    gtkdocize || exit $?
+fi
+
+
+#${AUTORECONF:-autoreconf} -fiv
+
+AUTORECONF=`which autoreconf`
+if test -z $AUTORECONF; then
+        echo "*** No autoreconf found, please install it ***"
+        exit 1
+else
+        autoreconf --force --install --verbose || exit $?
+fi
+
+cd "$olddir"
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
